1eb56737e8099ebe028b539de87417c7fa319cb1
turbobaguettes <kaspersky.kwinta@gmail.com>
2023-06-08 19:37:05 +0200

    ✨ mobile menu added for small devices

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 `use strict`;
+
+const mobileNavToggle = document.querySelector(`.mobile-nav-toggle`);
+const primaryNav = document.querySelector(`.primary-navigation`);
+
+mobileNavToggle.addEventListener("click", function () {
+	const visibility = primaryNav.getAttribute("data-visible");
+	if (visibility === "false") {
+		primaryNav.setAttribute("data-visible", true);
+		mobileNavToggle.setAttribute("aria-expanded", true);
+	} else {
+		primaryNav.setAttribute("data-visible", false);
+		mobileNavToggle.setAttribute("aria-expanded", false);
+	}
+});
